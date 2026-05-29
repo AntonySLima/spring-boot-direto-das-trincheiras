ba772eb529fb5e1935fe557ede2a53fbd0a44a9f
Antony de Souza Lima <contato.antonyslima@gmail.com>
2026-05-29 12:56:08 -0300

    video10: Virtual Threads, Stacktrace and Tomcat Threads

--- anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -1,15 +1,20 @@
 package academy.devdojo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("v1/animes")
+@Slf4j
 public class AnimeController {
 
-    @GetMapping("v1/animes")
+    @GetMapping()
     public List<String> listAll() {
+        log.info(Thread.currentThread().getName());
         return List.of("Attack on titan", "Berserk", "Jujutsu Kaisen");
     }
 }
