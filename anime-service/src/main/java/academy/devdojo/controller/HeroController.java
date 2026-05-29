4535ee640c1da7426a3150c15f8ef77cada1747d
Antony de Souza Lima <contato.antonyslima@gmail.com>
2026-05-29 20:13:41 -0300

    video12: PathVariable

---
+++ anime-service/src/main/java/academy/devdojo/controller/HeroController.java
@@ -0,0 +1,40 @@
+package academy.devdojo.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("v1/heroes")
+public class HeroController {
+    private static final List<String> HEROES = List.of("Batman", "Flash", "Superman", "Guts");
+
+    @GetMapping
+    public List<String> listAllHeroes() {
+        return HEROES;
+    }
+
+    @GetMapping("filter")
+    public List<String> listAllHeroesParam(@RequestParam(defaultValue = "") String name) {
+        return HEROES.stream()
+                .filter(hero -> hero.equalsIgnoreCase(name))
+                .toList();
+    }
+
+    @GetMapping("filterList")
+    public List<String> listAllHeroesParamList(@RequestParam(defaultValue = "") List<String> names) {
+        return HEROES.stream()
+                .filter(names::contains)
+                .toList();
+    }
+
+    @GetMapping("{name}")
+    public String findByName(@PathVariable String name) {
+        return HEROES.stream()
+                .filter(hero -> hero.equals(name))
+                .findAny().orElse("");
+    }
+}
